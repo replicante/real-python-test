6851a18c6c0817d1ffb8b2ca128ce477503390a5
jose tannhausser <lamiradadelreplicante@gmail.com>
2018-04-02 20:36:23 +0200

    flask-hello-word_update

--- app.py
+++ app.py
@@ -3,11 +3,14 @@
 
 #---------------Flask Hello World-----------------#
 
-#import the Flask class from the flask package
+# import the Flask class from the flask package
 from flask import Flask
 
-#create the applicatEion object
+# create the applicatEion object
 app = Flask(__name__)
+
+# error handling
+app.config["DEBUG"] = True
 
 # use the decorator patter to
 # link the view function to a url
@@ -16,9 +19,27 @@ app = Flask(__name__)
 
 #define the view using a function, which returns a string
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!?!?!?!"
+
+# dynamic route
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+# dynamic route with explicit status codes
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404
+
     
 # start the development server using the run() method
 if __name__ == "__main__":
     app.run()
+    
+
+
+
 
